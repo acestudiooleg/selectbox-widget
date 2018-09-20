54c822a3ada521fb58c8ddbeb927e645bb45e26b
Oleh Sudavnyi <osudav@softserveinc.com>
2018-09-20 23:23:24 +0300

    created selectbox component and added page

--- app/pods/components/select-box/component.js
+++ app/pods/components/select-box/component.js
@@ -1,4 +1,68 @@
 import Component from '@ember/component';
+import { observer } from '@ember/object';
+import { debounce } from '@ember/runloop';
+
+const filterData = (arr = [], optionKey, criteria = '') => new Promise((s) => {
+  const delayed = () =>
+    s(arr.filter(el => new RegExp(criteria, 'gi').test(optionKey ? el[optionKey] : el)));
+  setTimeout(delayed, 0);
+});
+const addTag = (arr = []) => arr.map((el, i) => Object.assign({}, el, {__id: i}));
+
+const cutIfNeeded = (arr = [], len) => arr.length > len ? arr.slice(0, len) : arr;
 
 export default Component.extend({
+  classNames: ['select-box'],
+  classNameBindings: ['isOpen:open'],
+  isOpen: false,
+  isLoading: false,
+  debounceTime: 350,
+  maxItemsToShow: 10,
+  optionsLength: null,
+  criteria: '',
+  placeholder: 'Please select ...',
+  didReceiveAttrs() {
+    this._super(...arguments);
+    const { options = [], maxItemsToShow } = this.getProperties('options', 'maxItemsToShow');
+    this.set('optionsLength', options.length > maxItemsToShow ? options.length : null);
+    const taggedOptions = addTag(cutIfNeeded(options, maxItemsToShow));
+    this.set('taggedOptions', taggedOptions);
+  },
+  click() {
+    return false;
+  },
+  filterOptions() {
+    const { options, optionKey, criteria } = this.getProperties('options', 'optionKey', 'criteria');
+    this.set('isLoading', true);
+    filterData(options, optionKey, criteria).then((data) => {
+      this.set('taggedOptions', addTag(cutIfNeeded(data)));
+      this.set('isLoading', false);
+    })
+  },
+  postKey: observer('criteria', function() {
+    debounce(this, this.filterOptions, this.get('debounceTime'));
+  }),
+  actions: {
+    open() {
+      this.set('isOpen', true);
+    },
+    close() {
+      this.set('isOpen', false);
+    },
+    select(option = {}) {
+      const { onChange, actions } = this.getProperties('onChange', 'actions');
+      onChange(option);
+      actions.close.call(this);
+    },
+    loadAll(){
+      this.set('isLoading', true);
+      const options = this.get('options');
+      (new Promise(resolve => setTimeout(() => resolve(addTag(options))), 0))
+        .then((taggedOptions) => {
+          this.set('taggedOptions', taggedOptions);
+          this.set('isLoading', false);
+        });
+
+    }
+  }
 });
--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -1,6 +1,24 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
+const devideByPattern = (arr = [], pattern) => new Promise((resolve) => {
+  const delayed = () => {
+    const servers = {};
+    arr.forEach(el => {
+      const [_, serverName] = el.match(pattern) || [];
+      if (serverName) {
+        if(!(serverName in servers)) {
+          servers[serverName] = [el];
+        } else {
+          servers[serverName].push(el);
+        }
+      }
+    });
+    resolve(servers);
+  };
+  setTimeout(delayed, 0);
+});
+
 export default Controller.extend({
   init() {
     this._super(...arguments);
@@ -10,9 +28,24 @@ export default Controller.extend({
   selectedSubCategory: null,
   categories: computed('model', function () {
     const model = this.get('model');
-    return Object.keys(model || {}).map(key => ({ name: key, value: model[key] }));
+    return this.get('convertToArray')(model);
   }),
-  setProperty(propName) {
-    return (value) => this.set(propName, value);
+  convertToArray(data) {
+    return (data instanceof Array) ?
+      data.map(name => ({ name, value: name })) :
+      Object.keys(data || {}).map(name => ({ name, value: data[name] }));
+  },
+  divideByServers(data) {
+    this.set('isLoading', true);
+    devideByPattern(data.value, new RegExp('://(.*?)/')).then((servers) => {
+      this.set('servers', servers);
+      this.set('isLoading', false);
+    });
+
+  },
+  setProperty(propName){
+    return (value) => {
+      this.set(propName, value);
+    }
   }
 });
