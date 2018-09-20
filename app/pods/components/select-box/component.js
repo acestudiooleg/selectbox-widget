b95ef7109d9e1bde78a1c6cf01e2fb0d08f4b547
Oleh Sudavnyi <osudav@softserveinc.com>
2018-09-20 23:59:43 +0300

    added documentation

--- app/pods/components/select-box/component.js
+++ app/pods/components/select-box/component.js
@@ -2,15 +2,47 @@ import Component from '@ember/component';
 import { observer } from '@ember/object';
 import { debounce } from '@ember/runloop';
 
+/**
+ * Filter array by regular expression
+ * @param  {Array} arr - options
+ * @param  {String} optionKey - key of object which located in array
+ * @param  {String} criteria - filter word (search)
+ * @return {Promise}
+ */
 const filterData = (arr = [], optionKey, criteria = '') => new Promise((s) => {
   const delayed = () =>
     s(arr.filter(el => new RegExp(criteria, 'gi').test(optionKey ? el[optionKey] : el)));
   setTimeout(delayed, 0);
 });
+
+
+/**
+ * Wrap each element by numeric __id property
+ * @param  {Array} arr - options
+ * @return {Array}
+ */
 const addTag = (arr = []) => arr.map((el, i) => Object.assign({}, el, {__id: i}));
 
+
+/**
+ * @module Components/SelectBox
+ * Cut array by length
+ * @param  {Array} arr - options
+ * @param  {Number} len - length of items which needs to left
+ * @return {Array}
+ */
 const cutIfNeeded = (arr = [], len) => arr.length > len ? arr.slice(0, len) : arr;
 
+/**
+ * @module Components/SelectBox
+ * @param {Boolean} isOpen - show dropdown list
+ * @param {Boolean} isLoading - Show spinner in dropdown list
+ * @param {Number} debounceTime - time between typing filter filed and reaction
+ * @param {Number} maxItemsToShow - length of items which need to show and other items remove from array
+ * @param {Number} optionsLength - length of list
+ * @param {String} criteria - filter word (search)
+ * @param {String} placeholder - Label for selectbox
+ */
 export default Component.extend({
   classNames: ['select-box'],
   classNameBindings: ['isOpen:open'],
@@ -23,14 +55,33 @@ export default Component.extend({
   placeholder: 'Please select ...',
   didReceiveAttrs() {
     this._super(...arguments);
+    this.tagAndCutOptions();
+  },
+
+
+  /**
+   * Cut drop down list by "maxItemsToShow" and added numeric __id for each object
+   */
+  tagAndCutOptions(){
     const { options = [], maxItemsToShow } = this.getProperties('options', 'maxItemsToShow');
     this.set('optionsLength', options.length > maxItemsToShow ? options.length : null);
     const taggedOptions = addTag(cutIfNeeded(options, maxItemsToShow));
     this.set('taggedOptions', taggedOptions);
   },
+
+
+  /**
+   * Prevent propagation
+   * @return {Boolean}
+   */
   click() {
     return false;
   },
+
+
+  /**
+   * filter options by search word
+   */
   filterOptions() {
     const { options, optionKey, criteria } = this.getProperties('options', 'optionKey', 'criteria');
     this.set('isLoading', true);
@@ -43,17 +94,38 @@ export default Component.extend({
     debounce(this, this.filterOptions, this.get('debounceTime'));
   }),
   actions: {
+
+
+    /**
+     * Show dropdown list
+     */
     open() {
       this.set('isOpen', true);
     },
+
+
+    /**
+     * Hide dropdown list
+     */
     close() {
       this.set('isOpen', false);
     },
+
+
+    /**
+     * Call parent function and pass selected option and closing dropdown list
+     * @param  {Object} option - selected item
+     */
     select(option = {}) {
       const { onChange, actions } = this.getProperties('onChange', 'actions');
       onChange(option);
       actions.close.call(this);
     },
+
+
+    /**
+     * Render all huge list to drop down list
+     */
     loadAll(){
       this.set('isLoading', true);
       const options = this.get('options');
--- app/pods/index/controller.js
+++ app/pods/index/controller.js
@@ -1,6 +1,13 @@
 import Controller from '@ember/controller';
 import { computed } from '@ember/object';
 
+/**
+ * @param  {String[]} arr
+ * @param  {RegExp} - pattern
+ * @return {Promise} - Object wich keys divided by pattern
+ * @example
+ * devideByPattern(['car', 'bar', 'arrow', 'some', 'other'], /(ar)/) // {ar: ['car', 'bar', 'arrow']}
+ */
 const devideByPattern = (arr = [], pattern) => new Promise((resolve) => {
   const delayed = () => {
     const servers = {};
@@ -19,6 +26,21 @@ const devideByPattern = (arr = [], pattern) => new Promise((resolve) => {
   setTimeout(delayed, 0);
 });
 
+/**
+ * @prop {Object} selectedCategory
+ * @prop {String} selectedCategory.name - category name
+ * @prop {Object} selectedCategory.value - subcategories
+ * @prop {Object} selectedSubCategory
+ * @prop {String} selectedSubCategory.name - subcategory name
+ * @prop {String[]} selectedSubCategory.value - URLs
+ * @prop {Object} selectedServer
+ * @prop {Object} selectedServer.name - server name
+ * @prop {String[]} selectedServer.value - URLs
+ * @prop {Object} selectedUrl
+ * @prop {Object} selectedUrl.name - url
+ * @prop {Object} selectedUrl.value - url
+ * @prop {Object[]} categories - [{ name, value }]
+ */
 export default Controller.extend({
   init() {
     this._super(...arguments);
@@ -26,23 +48,44 @@ export default Controller.extend({
   },
   selectedCategory: null,
   selectedSubCategory: null,
+  selectedServer: null,
+  selectedUrl: null,
   categories: computed('model', function () {
     const model = this.get('model');
     return this.get('convertToArray')(model);
   }),
+
+
+  /**
+   * convert object or array to next structure [{name, value}] - where name is key for object and name is value for array
+   * @param  {Array|Object}
+   * @return {Array}
+   */
   convertToArray(data) {
     return (data instanceof Array) ?
       data.map(name => ({ name, value: name })) :
       Object.keys(data || {}).map(name => ({ name, value: data[name] }));
   },
+
+
+  /**
+   * divide urls by server name - example.com: ['http://example.com/1', 'http://example.com/2', ...]
+   * @param  {String[]} - URLs
+   * @return {[type]}
+   */
   divideByServers(data) {
     this.set('isLoading', true);
     devideByPattern(data.value, new RegExp('://(.*?)/')).then((servers) => {
       this.set('servers', servers);
       this.set('isLoading', false);
     });
-
   },
+
+
+  /**
+   * @param {String} propName - property name of this controller
+   * @param {*} value - any value will be set by propName
+   */
   setProperty(propName){
     return (value) => {
       this.set(propName, value);
